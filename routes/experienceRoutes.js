fa801cb97394d4a9b168900f06754deab1b0b07c
adina142 <adinakhalid99@gmail.com>
2025-05-18 08:49:08 -0700

    Added experienceRoutes.js

--- routes/experienceRoutes.js
+++ routes/experienceRoutes.js
@@ -1,10 +1,21 @@
-// routes/educationRoutes.js
+// routes/experienceRoutes.js
 const express = require('express');
 const router = express.Router();
+const experienceController = require('../contollers/experienceContoller');
 
-// Example GET route
-router.get('/', (req, res) => {
-  res.send('Experience route working!');
-});
+// GET all experiences
+router.get('/', experienceController.getAllExperiences);
+
+// GET one experience by ID
+router.get('/:id', experienceController.getExperienceById);
+
+// POST new experience
+router.post('/', experienceController.createExperience);
+
+// PUT update experience
+router.put('/:id', experienceController.updateExperience);
+
+// DELETE experience
+router.delete('/:id', experienceController.deleteExperience);
 
 module.exports = router;
