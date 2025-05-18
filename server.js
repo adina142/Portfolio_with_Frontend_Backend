4b09ddfba30bd959a846fc8363dd203a685aa2b3
adina142 <adinakhalid99@gmail.com>
2025-05-18 08:56:56 -0700

    Update server.js-with ALL FUNCTIONALITIES

--- server.js
+++ server.js
@@ -4,24 +4,32 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const educationRoutes = require('./routes/educationRoutes');
 const skillRoutes = require('./routes/skillRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const experienceRoutes = require('./routes/experienceRoutes');
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-// Middleware
+// Middleware: allow frontend at localhost:3000 to access backend
 app.use(cors({ origin: 'http://localhost:3000' }));
+
+// Middleware: parse JSON bodies
 app.use(express.json());
 
 // Routes
 app.use('/api/education', educationRoutes);
-app.use('/api/skills', skillRoutes); // POST /api/skills
+app.use('/api/skills', skillRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/experience', experienceRoutes);
 
-// Root route (optional)
+
+// Root route - basic test endpoint
 app.get('/', (req, res) => {
   res.send('Portfolio API is running');
-});
+})
 
 // Start server
 const PORT = process.env.PORT || 5000;
